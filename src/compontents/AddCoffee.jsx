37fe06895be149ff8b63c63083294030f93ed5c3
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-27 23:27:18 +0600

    from

--- src/compontents/AddCoffee.jsx
+++ src/compontents/AddCoffee.jsx
@@ -2,9 +2,10 @@
 const AddCoffee = () => {
     return (
         <div>
-            <h1>add coffee</h1>
+
 
             <form className="bg-sky-500 w-10/12 mx-auto px-7 py-3">
+                <h1 className="text-3xl text-center text-black">add coffee</h1>
                 <div className="flex gap-4">
                     <div className="form-control w-[50%]">
                         <label className="label">
