662aa022f05ea4062622ddd5806b43612128321b
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-27 21:45:47 +0600

    clien

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Roots from './compontents/Roots';
-import Home from './compontents/Home';
-import Users from './compontents/Users';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Roots></Roots>,
-    children:[
-      {
-        path:'/',
-        element:<Home></Home>
-      },
-      {
-        path:'/users',
-        element:<Users></Users>,
-        loader:() =>fetch('http://localhost:5000/users')
-      }
-    ]
-  },
-]);
-
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 )
