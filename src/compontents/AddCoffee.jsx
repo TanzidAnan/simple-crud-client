3863a8d66f933405ea3d87c172a9a422d9085358
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-27 23:37:32 +0600

    name

--- src/compontents/AddCoffee.jsx
+++ src/compontents/AddCoffee.jsx
@@ -11,13 +11,13 @@ const AddCoffee = () => {
                         <label className="label">
                             <span className="label-text">Name</span>
                         </label>
-                        <input type="text" placeholder="Coffee Name" className="input input-bordered" required />
+                        <input type="text" name="name" placeholder="Coffee Name" className="input input-bordered" required />
                     </div>
                     <div className="form-control w-[50%]">
                         <label className="label">
                             <span className="label-text">Avable Quentaty</span>
                         </label>
-                        <input type="text" placeholder="Quentaty" className="input input-bordered" required />
+                        <input type="text" name="quentaty" placeholder="Quentaty" className="input input-bordered" required />
                     </div>
                 </div>
             </form>
