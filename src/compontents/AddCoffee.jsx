83b0bb872b3ac4a300aac0ef6acebc84361e1db3
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-27 23:40:06 +0600

    from style

--- src/compontents/AddCoffee.jsx
+++ src/compontents/AddCoffee.jsx
@@ -20,6 +20,34 @@ const AddCoffee = () => {
                         <input type="text" name="quentaty" placeholder="Quentaty" className="input input-bordered" required />
                     </div>
                 </div>
+                <div className="flex gap-4">
+                    <div className="form-control w-[50%]">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input type="text" name="name" placeholder="Coffee Name" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control w-[50%]">
+                        <label className="label">
+                            <span className="label-text">Avable Quentaty</span>
+                        </label>
+                        <input type="text" name="quentaty" placeholder="Quentaty" className="input input-bordered" required />
+                    </div>
+                </div>
+                <div className="flex gap-4">
+                    <div className="form-control w-[50%]">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input type="text" name="name" placeholder="Coffee Name" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control w-[50%]">
+                        <label className="label">
+                            <span className="label-text">Avable Quentaty</span>
+                        </label>
+                        <input type="text" name="quentaty" placeholder="Quentaty" className="input input-bordered" required />
+                    </div>
+                </div>
             </form>
         </div>
     );
