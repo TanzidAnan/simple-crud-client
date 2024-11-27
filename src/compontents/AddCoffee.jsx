7ef9506b1ea71858897124800e8a30fbce91a609
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-28 01:33:24 +0600

    from style

--- src/compontents/AddCoffee.jsx
+++ src/compontents/AddCoffee.jsx
@@ -1,10 +1,25 @@
 
 const AddCoffee = () => {
+
+    const hendleAddCoffee =(e) =>{
+        e.preventDefault();
+        const form =e.target;
+        const name =form.name.value;
+        const quentaty =form.quentaty.value;
+        const supplier =form.supplier.value;
+        const taste =form.taste.value;
+        const category =form.category.value;
+        const details =form.details.value;
+        const photo =form.photo.value;
+        const newCoffee ={name,quentaty,supplier,taste,category,details,photo}
+        console.log(newCoffee)
+    }
+
     return (
         <div className="mt-10">
 
 
-            <form className="bg-sky-100 w-10/12 mx-auto px-7 py-3 pb-10">
+            <form onSubmit={hendleAddCoffee} className="bg-sky-100 w-10/12 mx-auto px-7 py-3 pb-10">
                 <h1 className="text-3xl text-center text-black font-bold">Add coffee</h1>
                 <div className="flex gap-4">
                     <div className="form-control w-[50%]">
@@ -49,12 +64,16 @@ const AddCoffee = () => {
                     </div>
                 </div>
                 <div className="flex gap-4">
-                    <div className="form-control w-[50%]">
+                    <div className="form-control w-[100%]">
                         <label className="label">
                             <span className="label-text">photo</span>
                         </label>
                         <input type="text" name="photo" placeholder="photo" className="input input-bordered" required />
                     </div>
+                </div>
+                <div>
+                    {/* <button className="btn ">Add Coffee</button> */}
+                    <input className="btn btn-block mt-5" type="submit" value="add coffee" />
                 </div>
             </form>
         </div>
