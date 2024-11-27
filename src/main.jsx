116bd2815553e5a78415a77c159a0bca950fcf76
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-27 22:11:58 +0600

    add coffee

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,25 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import AddCoffee from './compontents/AddCoffee.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+  },
+  {
+    path:'addCoffee',
+    element:<AddCoffee></AddCoffee>
+  }
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </StrictMode>,
 )
