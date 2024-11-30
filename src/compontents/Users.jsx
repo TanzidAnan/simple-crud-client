66b0d9e7fd5d05fb4845e72efcdcf7175c791874
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-30 22:44:04 +0600

    table

--- src/compontents/Users.jsx
+++ src/compontents/Users.jsx
@@ -28,20 +28,6 @@ const Users = () => {
                             <td>Quality Control Specialist</td>
                             <td>Blue</td>
                         </tr>
-                        {/* row 2 */}
-                        <tr>
-                            <th>2</th>
-                            <td>Hart Hagerty</td>
-                            <td>Desktop Support Technician</td>
-                            <td>Purple</td>
-                        </tr>
-                        {/* row 3 */}
-                        <tr>
-                            <th>3</th>
-                            <td>Brice Swyre</td>
-                            <td>Tax Accountant</td>
-                            <td>Red</td>
-                        </tr>
                     </tbody>
                 </table>
             </div>
