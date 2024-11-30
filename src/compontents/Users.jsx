dd203a647a3bc7c971c2b0545899727168a1e579
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-30 22:41:24 +0600

    data

--- src/compontents/Users.jsx
+++ src/compontents/Users.jsx
@@ -1,11 +1,13 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
     const userData =useLoaderData();
     console.log(userData)
+    const [user,setUser] =useState(userData)
     return (
         <div>
-            <h1 className="text-4xl text-center">Users</h1>
+            <h1 className="text-4xl text-center">Users {user.length}</h1>
         </div>
     );
 };
