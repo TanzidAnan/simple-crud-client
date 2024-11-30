123ddfa691dd497d6e1946db4675e547b262efc5
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-30 22:43:33 +0600

    table

--- src/compontents/Users.jsx
+++ src/compontents/Users.jsx
@@ -2,12 +2,50 @@ import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
-    const userData =useLoaderData();
+    const userData = useLoaderData();
     console.log(userData)
-    const [user,setUser] =useState(userData)
+    const [user, setUser] = useState(userData)
     return (
         <div>
             <h1 className="text-4xl text-center">Users {user.length}</h1>
+
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        <tr>
+                            <th>1</th>
+                            <td>Cy Ganderton</td>
+                            <td>Quality Control Specialist</td>
+                            <td>Blue</td>
+                        </tr>
+                        {/* row 2 */}
+                        <tr>
+                            <th>2</th>
+                            <td>Hart Hagerty</td>
+                            <td>Desktop Support Technician</td>
+                            <td>Purple</td>
+                        </tr>
+                        {/* row 3 */}
+                        <tr>
+                            <th>3</th>
+                            <td>Brice Swyre</td>
+                            <td>Tax Accountant</td>
+                            <td>Red</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
         </div>
     );
 };
